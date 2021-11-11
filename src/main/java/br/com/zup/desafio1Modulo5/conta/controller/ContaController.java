bc9df78f3428f0066d880fb89b00f1ee19176846
Elaine <elaine.silva@zup.com.br>
2021-11-11 13:40:07 -0300

    "Criar metotos para mostrar contas codastradas"

--- src/main/java/br/com/zup/desafio1Modulo5/conta/controller/ContaController.java
+++ src/main/java/br/com/zup/desafio1Modulo5/conta/controller/ContaController.java
@@ -2,11 +2,15 @@ package br.com.zup.desafio1Modulo5.conta.controller;
 
 import br.com.zup.desafio1Modulo5.conta.model.Conta;
 import br.com.zup.desafio1Modulo5.conta.model.dtos.CadastroDeContaDTO;
+import br.com.zup.desafio1Modulo5.conta.model.dtos.ExibicaoDeContasDTO;
 import br.com.zup.desafio1Modulo5.conta.service.ContaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/contas")
@@ -24,6 +28,15 @@ public class ContaController {
         Conta conta = modelMapper.map(cadastroDeContaDTO, Conta.class);
         contaService.CadastrarConta(conta);
         return contaService.retornarContaPorID(conta.getId());
+    }
+
+    @GetMapping
+    public List<ExibicaoDeContasDTO> exibirContasCadastradas() {
+        List<ExibicaoDeContasDTO> contasDTOS = new ArrayList<>();
+        for (Conta conta : contaService.retornarContasCadastradas()) {
+            contasDTOS.add(modelMapper.map(conta, ExibicaoDeContasDTO.class));
+        }
+        return contasDTOS;
     }
 
 }
--- src/main/java/br/com/zup/desafio1Modulo5/conta/service/ContaService.java
+++ src/main/java/br/com/zup/desafio1Modulo5/conta/service/ContaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class ContaService {
@@ -24,6 +25,10 @@ public class ContaService {
         }
 
         contaRepository.save(conta);
+    }
+
+    public List<Conta> retornarContasCadastradas() {
+        return (List<Conta>) contaRepository.findAll();
     }
 
     public Conta retornarContaPorID(Integer id) {
