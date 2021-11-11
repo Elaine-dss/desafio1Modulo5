3e185b3b0ca9e2e3e6c6dae8630cf06b82b40a9d
Elaine <elaine.silva@zup.com.br>
2021-11-11 12:52:35 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/desafio1Modulo5/Desafio1Modulo5Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafio1Modulo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1Modulo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio1Modulo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/desafio1Modulo5/Desafio1Modulo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafio1Modulo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1Modulo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
