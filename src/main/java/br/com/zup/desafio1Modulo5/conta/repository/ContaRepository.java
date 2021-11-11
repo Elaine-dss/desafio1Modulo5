2d18d85f70096bd5f75b9c33d13ccd9ac2faa893
Elaine <elaine.silva@zup.com.br>
2021-11-11 13:17:55 -0300

    "Criar repository para conta"

---
+++ src/main/java/br/com/zup/desafio1Modulo5/conta/repository/ContaRepository.java
@@ -0,0 +1,10 @@
+package br.com.zup.desafio1Modulo5.conta.repository;
+
+import br.com.zup.desafio1Modulo5.conta.model.Conta;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContaRepository extends CrudRepository <Conta, Integer> {
+    
+}
