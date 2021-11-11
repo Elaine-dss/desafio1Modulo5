8fc3e7bc16a6dfd0bbc04140db723f8befd63d10
Elaine <elaine.silva@zup.com.br>
2021-11-11 13:30:20 -0300

    "Criar metodo para cadastrar conta"

--- src/main/java/br/com/zup/desafio1Modulo5/conta/service/ContaService.java
+++ src/main/java/br/com/zup/desafio1Modulo5/conta/service/ContaService.java
@@ -1,13 +1,29 @@
 package br.com.zup.desafio1Modulo5.conta.service;
 
+import br.com.zup.desafio1Modulo5.conta.enums.Status;
+import br.com.zup.desafio1Modulo5.conta.model.Conta;
 import br.com.zup.desafio1Modulo5.conta.repository.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class ContaService {
 
     @Autowired
     private ContaRepository contaRepository;
+
+    public void CadastrarConta(Conta conta){
+
+        if (conta.getDataDeVencimento().isBefore(LocalDate.now())) {
+            conta.setStatus(Status.VENCIDA);
+        }
+        else {
+            conta.setStatus(Status.AGUARDANDO);
+        }
+
+        contaRepository.save(conta);
+    }
 
 }
