31a6a4475552ed8e891c2bdf416f5ab3667dd33e
Elaine <elaine.silva@zup.com.br>
2021-11-12 13:46:14 -0300

    "Criar exceções"

---
+++ src/main/java/br/com/zup/desafio1Modulo5/conta/configs/exceptions/DadosDigitadosIncorretamente.java
@@ -0,0 +1,6 @@
+package br.com.zup.desafio1Modulo5.conta.configs.exceptions;
+
+public class DadosDigitadosIncorretamente extends RuntimeException {
+    public DadosDigitadosIncorretamente(String message) { super(message); }
+
+}
---
+++ src/main/java/br/com/zup/desafio1Modulo5/conta/configs/exceptions/SolicitacaoNaoEncontrada.java
@@ -0,0 +1,6 @@
+package br.com.zup.desafio1Modulo5.conta.configs.exceptions;
+
+public class SolicitacaoNaoEncontrada extends RuntimeException {
+    public SolicitacaoNaoEncontrada(String message) { super(message); }
+
+}
