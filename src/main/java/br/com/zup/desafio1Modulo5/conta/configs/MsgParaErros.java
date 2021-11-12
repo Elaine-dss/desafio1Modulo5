106746421d681d58312a79e62bbcf56e367ffdc9
Elaine <elaine.silva@zup.com.br>
2021-11-12 13:46:54 -0300

    "Criar classe para mensagens de erro"

---
+++ src/main/java/br/com/zup/desafio1Modulo5/conta/configs/MsgParaErros.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafio1Modulo5.conta.configs;
+
+public class MsgParaErros {
+
+    private String mensagem;
+
+    public MsgParaErros(String mensagem) { this.mensagem = mensagem; }
+
+    public String getMensagem() { return mensagem; }
+
+    public void setMensagem(String mensagem) { this.mensagem = mensagem; }
+
+}
